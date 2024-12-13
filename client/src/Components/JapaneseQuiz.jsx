c48c1a46263c7c00703e1bfdc341e99de37177d0
artjay <artjayvang@gmail.com>
2024-12-12 20:41:08 -0800

    Tidied up some code, will add modal tomorrow.

--- client/src/Components/JapaneseQuiz.jsx
+++ client/src/Components/JapaneseQuiz.jsx
@@ -20,6 +20,7 @@ export default function JapaneseQuiz() {
   const [gameStart, setGameStart] = useState(false);
   const [correctCount, setCorrectCount] = useState(0);
   const [isCorrect, setIsCorrect] = useState("");
+  const [openGameOver, setOpenGameOver] = useState(false);
   
 
   useEffect(() => {
@@ -43,6 +44,12 @@ export default function JapaneseQuiz() {
     setGameStart(true);
   }, [])
 
+  function checkWin() {
+    if (correctCount === japaneseContent.length) {
+      setOpenGameOver(true);
+    }
+  }
+
   function checkCorrect() {
     const japIdx = japaneseContent.findIndex(jap => jap === randomizedJap[japSelect]);
     const engIdx = englishContent.findIndex(eng => eng === randomizedEng[engSelect]);
@@ -57,60 +64,63 @@ export default function JapaneseQuiz() {
     }
     setJapSelect();
     setEngSelect();
+    checkWin();
   }
+
+  const MainGame = () => (
+    <div className="flex flex-col w-full min-h-screen items-center justify-center gap-14">
+      <h1 className="text-4xl font-medium">{title}</h1>
+
+      <h1 className="text-4xl font-medium">Score: {correctCount}/5</h1>
+
+      <div className="w-fit h-fit flex flex-col gap-20">
+        {/* Japanese Contents */}
+        <div className="flex gap-4">
+          {randomizedJap.map((jap, idx)  => (
+            <div key={idx}> 
+              <button 
+                className={`text-3xl border border-black py-2 px-4 rounded-lg ${(japSelect === idx || correctJap.includes(idx)) && "border-green-500 bg-green-200 pointer-events-none"} hover:bg-black hover:text-white `}
+                onClick={() => setJapSelect(idx)}
+              >
+                {jap}
+              </button>
+            </div>
+          ))}
+        </div>
+
+        {/* English Contents */}
+        <div className="flex gap-4">
+          {randomizedEng.map((eng, idx)  => (
+            <div key={idx}> 
+              <button 
+                className={`text-3xl border border-black py-2 px-4 rounded-lg ${(engSelect === idx || correctEng.includes(idx)) && "border-green-500 bg-green-200 pointer-events-none"} hover:bg-black hover:text-white`}
+                onClick={() => setEngSelect(idx)}
+              >
+                {eng}
+              </button>
+            </div>
+          ))}
+        </div>
+
+        <h1 className={`${isCorrect==="" && "invisible"} ${isCorrect ? "text-green-600" : "text-red-600"} text-3xl`}>{`${isCorrect ? "Correct!" : "Wrong! Try again."}`}</h1>
+
+        {/* Checks if the characters are correct */}
+        <button 
+          className="w-fit text-3xl border border-black py-2 px-4 rounded-lg hover:bg-black hover:text-white"
+          onClick={() => checkCorrect()}
+        >
+          Check
+        </button>
+      </div>
+    </div>
+  )
 
   // Loads the page
   if (!gameStart) {
     return <div>Loading...</div>
   } else {
   return (
-  
-  <div className="flex flex-col w-full min-h-screen items-center justify-center gap-14">
-    <h1 className="text-4xl font-medium">{title}</h1>
-
-    <h1 className="text-4xl font-medium">Score: {correctCount}/5</h1>
-
-    <div className="w-fit h-fit flex flex-col gap-20">
-      {/* Japanese Contents */}
-      <div className="flex gap-4">
-        {randomizedJap.map((jap, idx)  => (
-          <div key={idx}> 
-            <button 
-              className={`text-3xl border border-black py-2 px-4 rounded-lg ${(japSelect === idx || correctJap.includes(idx)) && "border-green-500 bg-green-200 pointer-events-none"} hover:bg-black hover:text-white `}
-              onClick={() => setJapSelect(idx)}
-            >
-              {jap}
-            </button>
-          </div>
-        ))}
-      </div>
-
-      {/* English Contents */}
-      <div className="flex gap-4">
-        {randomizedEng.map((eng, idx)  => (
-          <div key={idx}> 
-            <button 
-              className={`text-3xl border border-black py-2 px-4 rounded-lg ${(engSelect === idx || correctEng.includes(idx)) && "border-green-500 bg-green-200 pointer-events-none"} hover:bg-black hover:text-white`}
-              onClick={() => setEngSelect(idx)}
-            >
-              {eng}
-            </button>
-          </div>
-        ))}
-      </div>
-
-      <h1 className={`${isCorrect==="" && "invisible"} ${isCorrect ? "text-green-600" : "text-red-600"} text-3xl`}>{`${isCorrect ? "Correct!" : "Wrong! Try again."}`}</h1>
-
-      {/* Checks if the characters are correct */}
-      <button 
-        className="w-fit text-3xl border border-black py-2 px-4 rounded-lg hover:bg-black hover:text-white"
-        onClick={() => checkCorrect()}
-      >
-        Check
-      </button>
-
-    </div>
-  </div>
+    <MainGame />
   );
 }
 }
